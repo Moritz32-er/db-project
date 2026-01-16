e5add4fe9915095fab263ced2c95d782ca787f73
Mahmutabi <anasjindi@hotmail.com>
2026-01-16 20:38:59 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,111 +3,73 @@ from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import db_read, db_write
 
-# Logger für dieses Modul
 logger = logging.getLogger(__name__)
-
 login_manager = LoginManager()
 
 
 class User(UserMixin):
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password_hash):
         self.id = id
         self.username = username
-        self.password = password
+        self.password = password_hash  # Flask-Login erwartet .password hier
+
 
     @staticmethod
     def get_by_id(user_id):
-        logger.debug("User.get_by_id() aufgerufen mit user_id=%s", user_id)
-        try:
-            row = db_read(
-                "SELECT * FROM users WHERE id = %s",
-                (user_id,),
-                single=True
-            )
-            logger.debug("User.get_by_id() DB-Ergebnis: %r", row)
-        except Exception:
-            logger.exception("Fehler bei User.get_by_id(%s)", user_id)
+        logger.debug("User.get_by_id(%s)", user_id)
+        row = db_read(
+            "SELECT id, username, password_hash FROM users WHERE id=%s",
+            (user_id,),
+            single=True
+        )
+        if not row:
+            logger.warning("Kein User mit id=%s", user_id)
             return None
 
-        if row:
-            return User(row["id"], row["username"], row["password"])
-        else:
-            logger.warning("User.get_by_id(): kein User mit id=%s gefunden", user_id)
-            return None
+        return User(row["id"], row["username"], row["password_hash"])
+
 
     @staticmethod
     def get_by_username(username):
-        logger.debug("User.get_by_username() aufgerufen mit username=%s", username)
-        try:
-            row = db_read(
-                "SELECT * FROM users WHERE username = %s",
-                (username,),
-                single=True
-            )
-            logger.debug("User.get_by_username() DB-Ergebnis: %r", row)
-        except Exception:
-            logger.exception("Fehler bei User.get_by_username(%s)", username)
+        logger.debug("User.get_by_username(%s)", username)
+        row = db_read(
+            "SELECT id, username, password_hash FROM users WHERE username=%s",
+            (username,),
+            single=True
+        )
+        if not row:
             return None
 
-        if row:
-            return User(row["id"], row["username"], row["password"])
-        else:
-            logger.info("User.get_by_username(): kein User mit username=%s", username)
-            return None
+        return User(row["id"], row["username"], row["password_hash"])
 
 
-# Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
-    logger.debug("load_user() aufgerufen mit user_id=%s", user_id)
     try:
-        user = User.get_by_id(int(user_id))
-    except ValueError:
-        logger.error("load_user(): user_id=%r ist keine int", user_id)
+        return User.get_by_id(int(user_id))
+    except Exception:
+        logger.exception("load_user failed")
         return None
 
-    if user:
-        logger.debug("load_user(): User gefunden: %s (id=%s)", user.username, user.id)
-    else:
-        logger.warning("load_user(): kein User für id=%s gefunden", user_id)
 
-    return user
-
-
-# Helpers
 def register_user(username, password):
-    logger.info("register_user(): versuche neuen User '%s' anzulegen", username)
-
-    existing = User.get_by_username(username)
-    if existing:
-        logger.warning("register_user(): Username '%s' existiert bereits", username)
+    if User.get_by_username(username):
         return False
 
-    hashed = generate_password_hash(password)
-    try:
-        db_write(
-            "INSERT INTO users (username, password) VALUES (%s, %s)",
-            (username, hashed)
-        )
-        logger.info("register_user(): User '%s' erfolgreich angelegt", username)
-    except Exception:
-        logger.exception("Fehler beim Anlegen von User '%s'", username)
-        return False
-
+    password_hash = generate_password_hash(password)
+    db_write(
+        "INSERT INTO users (username, password_hash) VALUES (%s, %s)",
+        (username, password_hash)
+    )
     return True
 
 
 def authenticate(username, password):
-    logger.info("authenticate(): Login-Versuch für '%s'", username)
     user = User.get_by_username(username)
-
     if not user:
-        logger.warning("authenticate(): kein User mit username='%s' gefunden", username)
         return None
 
     if check_password_hash(user.password, password):
-        logger.info("authenticate(): Passwort korrekt für '%s'", username)
         return user
 
-    logger.warning("authenticate(): falsches Passwort für '%s'", username)
     return None
